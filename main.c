331729a5768577f42dadb5c7a404b94ebdb2eb7f
Amith S Dev <70939150+AmithSahadevan@users.noreply.github.com>
2023-04-02 23:53:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //to display something on the screen, use printf(""message to be displayed")//
+    printf("Hello world!\n");
+    //in case you wanna display something after a tab space, use \t //
+    printf("hello\tworld\n"); // always make sure to ad ; after the line of code //
+    // you can use \n when you wanna display something in a new line //
+    printf("hello\nworld");
+
+    return 0;
+}
